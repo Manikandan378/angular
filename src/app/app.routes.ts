6e8901e25514d1e61a96d969f6edb87df090b8df
Manikandan1368 <manikandanangdev68@gmail.com>
2025-05-21 17:50:40 +0530

    'newbuild'

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ViewcompComponent } from './viewcomp/viewcomp.component';
-
-export const routes: Routes = [
-        { path: '', component: ViewcompComponent },
-];
